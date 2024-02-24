7704d8bbc8933e6407b3293e951a36bc7bf6f1d7
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-02-24 19:19:50 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBoot3WebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3WebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3WebsocketApplication.class, args);
+	}
+}
